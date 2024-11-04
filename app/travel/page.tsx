432852a5f59d3c1eb534bdb85f39781497885f24
Γιαννης Ζουμπουλακης <gzoumpoulakis@loggia.net>
2024-11-04 18:42:17 +0200

    f

--- app/travel/page.tsx
+++ app/travel/page.tsx
@@ -102,7 +102,7 @@ export default function ChatBot() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
                 </svg>
-                <div className="text">
+                <div className="text hide">
                 Send
                 </div>
                 </button>
